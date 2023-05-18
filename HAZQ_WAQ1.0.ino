fdf1ddc2a9d4ac076f79c9c678d8a668119e5daf
JoshBaptist <77997125+JoshBaptist@users.noreply.github.com>
2023-05-18 11:31:20 -0600

    Version2 - Optimized

--- HAZQ_WAQ1.0.ino
+++ HAZQ_WAQ1.0.ino
@@ -1,343 +1,400 @@
-
-#include "Adafruit_ILI9341.h"
+//comment out the following line to use TFT screen instead of SD card
+//#define USE_SD_CARD 1
 
 
 #include "Nicla_System.h"
-#include "sps30.h"
-#include <Wire.h>
-
-#define SP30_COMMS Wire
-
-
-#define DEBUG 0
-
-void serialTrigger(char * mess);
-void ErrtoMess(char *mess, uint8_t r);
-void Errorloop(char *mess, uint8_t r);
-void GetDeviceInfo();
-bool read_all();
-
-
-SPS30 sps30;
 #include <Adafruit_GPS.h>
-
-Adafruit_GPS GPS(&Wire);
-
-#define GPSECHO false
-
-uint32_t timer = millis();
-
+#include "sps30.h"
 #include "Arduino_BHY2.h"
-Sensor gas(SENSOR_ID_GAS);
-SensorConfig cfg;
-struct sps_values spsData;
+#include <Wire.h>
+#include "Adafruit_ILI9341.h"
+#include <SPI.h>
+#include <SD.h>
+#ifdef USE_SD_CARD
+#include "Sd2PinMap.h"
+#endif
 
-//#include <SensirionI2CScd4x.h>
-//SensirionI2CScd4x scd4x;
-
-
+#ifndef USE_SD_CARD
+//if we are not using the SD card, then we are using the TFT
 #define TFT_CS 6
 #define TFT_DC 5
 #define TFT_MOSI 7
 #define TFT_CLK 9
-#define TFT_RST 0
+#define TFT_RST A0
 #define TFT_MISO 8
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
-#include "SparkFun_SCD4x_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_SCD4x
-SCD4x mySensor;
+#endif
 
-//const int buttonPin = 1;
-//int buttonState = 0;
+Adafruit_GPS GPS(&Wire);
+#define GPSECHO false
 
+uint32_t timer = millis();
+
+Sensor gas(SENSOR_ID_GAS);
+SPS30 sps30;
+sps_values val;
+
+const int chipSelect = 0;
+
+#ifdef USE_SD_CARD
+File dataFile;
+
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+#endif
 
 void setup() {
   nicla::begin();
-  BHY2.begin();
-  gas.begin();
   Serial.begin(115200);
+  Wire.begin();
   GPS.begin(0x10);  // The I2C address to use is 0x10
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);  // 1 Hz update rate
   GPS.sendCommand(PGCMD_ANTENNA);
+  // delay(1000);
   GPS.println(PMTK_Q_RELEASE);
-  sps30.EnableDebugging(DEBUG);
-  SP30_COMMS.begin();
+  BHY2.begin();
+  // delay(500);
+  BHY2.begin();
+  gas.begin();
+  sps30.begin(&Wire);
 
-//  pinMode(buttonPin, Input)
-
-  if (sps30.begin(&SP30_COMMS) == false) {
-    Errorloop((char *) "Could not set I2C communication channel.", 0);
-  }
-  if (! sps30.probe()) Errorloop((char *) "could not probe / connect with SPS30.", 0);
-  if (! sps30.reset()) Errorloop((char *) "could not reset.", 0);
+#ifndef USE_SD_CARD
   tft.begin();
   tft.fillScreen(ILI9341_BLACK);
-}
-
-
-
-void loop() {
-    BHY2.update();
-
-
- char c = GPS.read();
- 
- //if (GPSECHO)
-  //if (c) Serial.print(c);
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-    //Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-      return; // we can fail to parse a sentence in which case we should just wait for another
-
-        mySensor.begin();
-//        if (mySensor.readMeasurement()) {
-////            SCD41TFT();
-//    Serial.print(F("CO2(ppm):"));
-//    Serial.print(mySensor.getCO2());
-//        }
-//            mySensor.stopPeriodicMeasurement();
-
-          tft.setCursor(0, 62);
-          tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-          tft.setTextSize(2);
-          tft.print("VOC_Rel: ");
-          tft.print(gas.value());
-          
-
-  if (millis() - timer > 2000) {
-    timer = millis(); // reset the timer
-//    Serial.println(String("gas: ") + String(gas.value(),3));
-    //GPSSerial1();
-    
-    if (GPS.fix) {
-      //GPSSerial2();
-      GPSTFT();     
-    }
-    read_all();
-    TabularTFT();
-    SCD41TFT();
-
-  }
-}
-}
-
-
-bool read_all()
-{
-  uint8_t ret, error_cnt = 0;
-  struct sps_values val;
-
-  // loop to get data
-  do {
-
-#ifdef USE_50K_SPEED                // update 1.4.3
-    SP30_COMMS.setClock(50000);     // set to 50K
-    ret = sps30.GetValues(&val);
-    SP30_COMMS.setClock(100000);    // reset to 100K in case other sensors are on the same I2C-channel
-#else
-    ret = sps30.GetValues(&val);
-
 #endif
 
-    // data might not have been ready
-    if (ret == SPS30_ERR_DATALENGTH){
+#ifdef USE_SD_CARD
+  if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("1. is a card inserted?");
+    Serial.println("2. is your wiring correct?");
+    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
+    Serial.println("Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!");
+    while (true)
+      ;
+  }
 
-        if (error_cnt++ > 3) {
-          ErrtoMess((char *) "Error during reading values: ",ret);
-          return(false);
-        }
-        delay(1000);
-    }
-
-    // if other error
-    else if(ret != SPS30_ERR_OK) {
-      ErrtoMess((char *) "Error during reading values: ",ret);
-      return(false);
-    }
-
-  } while (ret != SPS30_ERR_OK);
-
-  spsData=val;
-  return(true);
+  // Open the data file
+  dataFile = SD.open("datalog.txt", FILE_WRITE);
+#endif
 }
 
-void Errorloop(char *mess, uint8_t r)
-{
-  if (r) ErrtoMess(mess, r);
-  else Serial.println(mess);
-  Serial.println(F("Program on hold"));
-  for(;;) delay(100000);
-}
+int year = 0;
+int month = 0;
+int day = 0;
+int hour = 0;
+int minute = 0;
+int seconds = 0;
+float latitude = 0;
+float longitude = 0;
+char lat = 'X';
+char lon = 'X';
+float CO2 = 0;
+float temperature = 0;
+float humidity = 0;
 
-void ErrtoMess(char *mess, uint8_t r)
-{
-  char buf[80];
 
-  Serial.print(mess);
+void TFT() {
+//TFT Print//
+#ifndef USE_SD_CARD
+  tft.setCursor(0, 0);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.print("Lat: ");
+  tft.print(latitude / 100, 7);
+  tft.print(lat);
 
-  sps30.GetErrDescription(r, buf, 80);
-  Serial.println(buf);
-}
+  tft.setCursor(0, 21);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.print("Lon: ");
+  tft.print(longitude / 100, 7);
+  tft.print(lon);
 
-void TabularTFT()
-{
   tft.setCursor(0, 82);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
   tft.print("M_PM1: ");
-  tft.print(spsData.MassPM1);  
+  tft.print(val.MassPM1);
   tft.setCursor(0, 102);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
   tft.print("M_PM2: ");
-  tft.print(spsData.MassPM2);
+  tft.print(val.MassPM2);
   tft.setCursor(0, 122);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
   tft.print("M_PM4: ");
-  tft.print(spsData.MassPM4);
+  tft.print(val.MassPM4);
   tft.setCursor(0, 142);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
   tft.print("M_PM10: ");
-  tft.print(spsData.MassPM10);  
+  tft.print(val.MassPM10);
   tft.setCursor(0, 162);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
   tft.print("N_PM1: ");
-  tft.print(spsData.NumPM1);
+  tft.print(val.NumPM1);
   tft.setCursor(0, 182);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
   tft.print("N_PM2: ");
-  tft.print(spsData.NumPM2);  
+  tft.print(val.NumPM2);
   tft.setCursor(0, 202);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
   tft.print("N_PM4: ");
-  tft.print(spsData.NumPM4);
+  tft.print(val.NumPM4);
   tft.setCursor(0, 222);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
   tft.print("N_PM10: ");
-  tft.print(spsData.NumPM10); 
+  tft.print(val.NumPM10);
   tft.setCursor(0, 242);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
   tft.print("Particle Size: ");
-  tft.print(spsData.PartSize);
+  tft.print(val.PartSize);
+
+  tft.setCursor(0, 62);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.print("VOC_Rel: ");
+  tft.print(gas.value());
+#endif
 }
 
-
-void TabularSerial()
-{
-  Serial.print(spsData.MassPM1);
-  Serial.print(F("\t"));
-  Serial.print(spsData.MassPM2);
-  Serial.print(F("\t"));
-  Serial.print(spsData.MassPM4);
-  Serial.print(F("\t"));
-  Serial.print(spsData.MassPM10);
-  Serial.print(F("\t"));
-  Serial.print(spsData.NumPM0);
-  Serial.print(F("\t"));
-  Serial.print(spsData.NumPM1);
-  Serial.print(F("\t"));
-  Serial.print(spsData.NumPM2);
-  Serial.print(F("\t"));
-  Serial.print(spsData.NumPM4);
-  Serial.print(F("\t"));
-  Serial.print(spsData.NumPM10);
-  Serial.print(F("\t"));
-  Serial.print(spsData.PartSize);
-  Serial.print(F("\n"));
-}
-
-void GPSTFT(){
+void GPSTFT() {
+  //TFT Print//
+#ifndef USE_SD_CARD
   tft.setCursor(0, 0);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.print("Latitude: ");
-  tft.print(GPS.latitude, 3);
+  tft.print("Lat: ");
+  tft.print(latitude, 3);
+  tft.print(lat);
 
-  //display longitude
   tft.setCursor(0, 21);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.print("Longitude: ");
-  tft.print(GPS.longitude, 3);
-    
-  tft.setCursor(0, 42);
-  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-  tft.setTextSize(2);
-  tft.print("Altitude: ");
-  tft.print(GPS.altitude);
-
+  tft.print("Lon: ");
+  tft.print(longitude, 3);
+  tft.print(lon);
+#endif
 }
 
-void SCD41TFT(){
-  mySensor.readMeasurement(); 
-
-    Serial.print(F("CO2(ppm):"));
-    Serial.print(mySensor.getCO2());
-        
-
-            
-  tft.setCursor(0, 262);
+void DATATFT() {
+#ifndef USE_SD_CARD
+  tft.setCursor(0, 82);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.print("CO2: ");
-  tft.print(mySensor.getCO2());
-
-  //display longitude
-  tft.setCursor(0, 282);
+  tft.print("M_PM1: ");
+  tft.print(val.MassPM1);
+  tft.setCursor(0, 102);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.print("Temp: ");
-  tft.print(mySensor.getTemperature(), 1);
-    
-  tft.setCursor(0, 302);
+  tft.print("M_PM2: ");
+  tft.print(val.MassPM2);
+  tft.setCursor(0, 122);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.print("Humidity: ");
-  tft.print(mySensor.getHumidity(), 1);
-              mySensor.stopPeriodicMeasurement();
+  tft.print("M_PM4: ");
+  tft.print(val.MassPM4);
+  tft.setCursor(0, 142);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.print("M_PM10: ");
+  tft.print(val.MassPM10);
+  tft.setCursor(0, 162);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.print("N_PM1: ");
+  tft.print(val.NumPM1);
+  tft.setCursor(0, 182);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.print("N_PM2: ");
+  tft.print(val.NumPM2);
+  tft.setCursor(0, 202);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.print("N_PM4: ");
+  tft.print(val.NumPM4);
+  tft.setCursor(0, 222);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.print("N_PM10: ");
+  tft.print(val.NumPM10);
+  tft.setCursor(0, 242);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.print("Particle Size: ");
+  tft.print(val.PartSize);
 
-        
+  tft.setCursor(0, 62);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.print("VOC_Rel: ");
+  tft.print(gas.value());
+#endif
 }
 
-void GPSSerial1(){
 
-//Serial.print("\nTime: ");
-    //if (GPS.hour < 10) { Serial.print('0'); }
-    //Serial.print(GPS.hour, DEC); Serial.print(':');
-    //if (GPS.minute < 10) { Serial.print('0'); }
-    //Serial.print(GPS.minute, DEC); Serial.print(':');
-    //if (GPS.seconds < 10) { Serial.print('0'); }
-    //Serial.print(GPS.seconds, DEC); Serial.print('.');
-   // if (GPS.milliseconds < 10) {
-   //   Serial.print("00");
-    //} else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
-    //  Serial.print("0");
-    //}
-    //Serial.println(GPS.milliseconds);
-    //Serial.print("Date: ");
-    //Serial.print(GPS.day, DEC); Serial.print('/');
-    //Serial.print(GPS.month, DEC); Serial.print("/20");
-    //Serial.println(GPS.year, DEC);
-    //Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    //Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-}
+void loop() {
+#ifdef USE_SD_CARD
+  SD.end();
+#endif
 
-void GPSSerial2(){
-//  Serial.print("Location: ");
-    //  Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-        //  Serial.print(", ");
-    //  Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-    //  Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-   //   Serial.print("Angle: "); Serial.println(GPS.angle);
-    //  Serial.print("Altitude: "); Serial.println(GPS.altitude);
-    //  Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+  BHY2.update();
+  char c = GPS.read();
+  if (GPS.newNMEAreceived()) {
+    if (GPS.parse(GPS.lastNMEA())) {  // this also sets the newNMEAreceived() flag to false
+      if (GPS.fix) {
+        year = 2000 + GPS.year;
+        month = GPS.month;
+        day = GPS.day;
+        hour = GPS.hour;
+        minute = GPS.minute;
+        seconds = GPS.seconds;
+        latitude = GPS.latitude;
+        longitude = GPS.longitude;
+        lat = GPS.lat;  //N/S
+        lon = GPS.lon;  //E/W
+        TFT();
+        // GPSTFT();
+        // DATATFT();
+      }
+    }
+  }
+
+  if (millis() - timer > 2000) {
+    timer = millis();  // reset the timer
+    sps30.GetValues(&val);
+
+    static boolean headerprinted = false;
+    if (!headerprinted) {
+      Serial.println("Year,Month,Day,Hour,Minute,Second,Lat,NS,Lon,EW,P1.0,P2.5,P4.0,P10,P0.5,P1.0,P2.5,P4.0,P10,PartSize,VOCs,CO2,Temperature,Humidity");
+      headerprinted = true;
+    }
+
+    Serial.print(year);
+    Serial.print(",");
+    Serial.print(month);
+    Serial.print(",");
+    Serial.print(day);
+    Serial.print(",");
+    Serial.print(hour);
+    Serial.print(",");
+    Serial.print(minute);
+    Serial.print(",");
+    Serial.print(seconds);
+    Serial.print(",");
+    Serial.print(latitude / 100.0, 10);
+    Serial.print(",");
+    Serial.print(lat);
+    Serial.print(",");
+    Serial.print(longitude / 100.0, 10);
+    Serial.print(",");
+    Serial.print(lon);
+    Serial.print(",");
+
+    Serial.print(val.MassPM1);
+    Serial.print(",");
+    Serial.print(val.MassPM2);
+    Serial.print(",");
+    Serial.print(val.MassPM4);
+    Serial.print(",");
+    Serial.print(val.MassPM10);
+    Serial.print(",");
+    Serial.print(val.NumPM0);
+    Serial.print(",");
+    Serial.print(val.NumPM1);
+    Serial.print(",");
+    Serial.print(val.NumPM2);
+    Serial.print(",");
+    Serial.print(val.NumPM4);
+    Serial.print(",");
+    Serial.print(val.NumPM10);
+    Serial.print(",");
+    Serial.print(val.PartSize);
+    Serial.print(",");
+    Serial.print(gas.value());
+    //Print filler for CO2, Temperature, Humidity
+    Serial.print(",");
+    Serial.print("0");
+    Serial.print(",");
+    Serial.print("0");
+    Serial.print(",");
+    Serial.print("0");
+    Serial.println();
+
+#ifdef USE_SD_CARD
+    if (dataFile) {
+      SD.begin(chipSelect);
+
+      static boolean headerprinted = false;
+      if (!headerprinted) {
+        dataFile.println("Year,Month,Day,Hour,Minute,Second,Lat,NS,Lon,EW,P1.0,P2.5,P4.0,P10,P0.5,P1.0,P2.5,P4.0,P10,PartSize,VOCs,CO2,Temperature,Humidity");
+        headerprinted = true;
+      }
+
+      dataFile.print(year);
+      dataFile.print(",");
+      dataFile.print(month);
+      dataFile.print(",");
+      dataFile.print(day);
+      dataFile.print(",");
+      dataFile.print(hour);
+      dataFile.print(",");
+      dataFile.print(minute);
+      dataFile.print(",");
+
+      dataFile.print(seconds);
+      dataFile.print(",");
+      dataFile.print(latitude / 100.0, 10);
+      dataFile.print(",");
+      dataFile.print(lat);
+      dataFile.print(",");
+      dataFile.print(longitude / 100.0, 10);
+      dataFile.print(",");
+      dataFile.print(lon);
+      dataFile.print(",");
+
+      dataFile.print(val.MassPM1);
+      dataFile.print(",");
+      dataFile.print(val.MassPM2);
+      dataFile.print(",");
+      dataFile.print(val.MassPM4);
+      dataFile.print(",");
+      dataFile.print(val.MassPM10);
+      dataFile.print(",");
+      dataFile.print(val.NumPM0);
+      dataFile.print(",");
+      dataFile.print(val.NumPM1);
+      dataFile.print(",");
+      dataFile.print(val.NumPM2);
+      dataFile.print(",");
+      dataFile.print(val.NumPM4);
+      dataFile.print(",");
+      dataFile.print(val.NumPM10);
+      dataFile.print(",");
+      dataFile.print(val.PartSize);
+      dataFile.print(",");
+      dataFile.print(gas.value());
+      //Print filler for CO2, Temperature, Humidity
+      dataFile.print(",");
+      dataFile.print("0");
+      dataFile.print(",");
+      dataFile.print("0");
+      dataFile.print(",");
+      dataFile.print("0");
+      dataFile.println();
+      dataFile.flush();
+    }
+#endif
+  }
 }
